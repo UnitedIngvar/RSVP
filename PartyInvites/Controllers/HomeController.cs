9b9e991ce7beb889d5ef7318f2a6e8944f8a5b89
Игорь Петров <ingvarpetrova@gmail.com>
2021-03-18 17:04:11 +0300

    done everything

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+using System.Linq;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponce)
+        {
+            // TODO: Store responce from guest
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponce);
+                return View("Thanks", guestResponce);
+            }
+            else
+                return View();
+        }
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
+    }
+}
